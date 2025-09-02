837974843a7a81b1a71079194a88f4beaf5a2d98
Abhishek Krishna <abhishekkrishna@Abhisheks-MacBook-Air.local>
2025-09-02 23:45:30 +0530

    for brand and cli

--- .next/static/chunks/Desktop_prod_voidcore_app_page_tsx_170ec8c9._.js
+++ .next/static/chunks/Desktop_prod_voidcore_app_page_tsx_170ec8c9._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/b9844__pnpm_0b4d57de._.js",
+  "static/chunks/b9844__pnpm_cb1bece9._.js",
   "static/chunks/Desktop_prod_voidcore_app_page_tsx_2148cc8a._.js"
 ],
     source: "dynamic"

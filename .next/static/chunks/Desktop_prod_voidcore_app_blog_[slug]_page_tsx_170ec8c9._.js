2d4297558a768a7ee7bd822fadee48ee2c2b18b7
Abhishek Krishna <abhishekkrishna@Abhisheks-MacBook-Air.local>
2025-09-13 14:26:42 +0530

    fixed env secret

--- .next/static/chunks/Desktop_prod_voidcore_app_blog_[slug]_page_tsx_170ec8c9._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/Desktop_prod_voidcore_47fb07fb._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/app_page_tsx_ef62684a._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_53f4da5b._.js",
-  "static/chunks/_07a5fd53._.js"
-],
-    source: "dynamic"
-});

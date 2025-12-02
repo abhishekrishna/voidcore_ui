0bb9101b263b5e7656979cbca1a3f2bdfe70796a
Abhishek Krishna <abhishekkrishna@Abhisheks-MacBook-Air.local>
2025-12-02 14:57:06 +0530

    removed pricing

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { SplinePointer, Menu, X } from "lucide-react";
+import { Menu, X } from "lucide-react";
 import Link from "next/link";
 
 export default function Navbar() {

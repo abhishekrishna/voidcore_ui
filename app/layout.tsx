560fc17e08587ae6d889f4bb2ef937b106baa22a
Abhishek Krishna <abhishekkrishna@Abhisheks-MacBook-Air.local>
2026-01-08 20:18:06 +0530

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ThemeProvider from "@/components/theme-provider";
@@ -23,8 +24,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head />
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider>{children}</ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
-
 }

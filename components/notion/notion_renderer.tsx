6a5f0262e49aca322c0508fdeb205be322354656
Abhishek Krishna <abhishekkrishna@Abhisheks-MacBook-Air.local>
2026-01-12 13:42:41 +0530

    added code inline block

--- components/notion/notion_renderer.tsx
+++ components/notion/notion_renderer.tsx
@@ -2,7 +2,9 @@
 
 import { NotionRenderer } from "react-notion-x";
 import "react-notion-x/src/styles.css";
-import "prismjs/themes/prism.css"; // light theme for code
+import "prismjs/themes/prism.css";      // light theme
+import "prismjs/themes/prism-okaidia.css"; // dark theme
+import { Code } from "react-notion-x/build/third-party/code";
 import type { ExtendedRecordMap } from "notion-types";
 
 interface NotionPageRendererProps {
@@ -17,14 +19,16 @@ export default function NotionPageRenderer({
   if (!recordMap) return null;
 
   return (
-    <div className="notion dark:notion-dark">
+    <div className={`notion ${darkMode ? "dark:notion-dark" : ""}`}>
       <NotionRenderer
-      recordMap={recordMap}
-      fullPage={false}
-      darkMode={darkMode}
-      components={{
-        Collection: () => null,
-      }}
-    /></div>
+        recordMap={recordMap}
+        fullPage={false}
+        darkMode={darkMode}
+        components={{
+          Collection: () => null,
+          Code: Code, // THIS IS THE KEY: properly render code blocks
+        }}
+      />
+    </div>
   );
 }
